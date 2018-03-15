6eaf0009b02cbe3fbea0b0256c63313571573ca2
madhavipathapati <madhavi.pathapati@olivetech.net>
2018-03-15 14:19:29 +0530

    first commit

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,166 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var userService = require('services/user.service');
+var nodemailer = require('nodemailer');
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/current', getCurrent);
+router.put('/:_id', update);
+router.delete('/:_id', _delete);
+
+module.exports = router;
+
+function authenticate(req, res) {
+    userService.authenticate(req.body.username, req.body.password)
+        .then(function (user) {
+            if (user) {
+                // authentication successful
+                res.send(user);
+                const output = `<p>You have Employees request</p>
+                <h3>Employees Deatils</h3>
+                <ul>
+                <li>Madhavi Employee Details</li>
+                </ul>
+                <h3>Meaasge</h3>
+               
+                `;
+                let transporter = nodemailer.createTransport({
+                    host: 'smtp.gmail.com',
+                    port: 25,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                        user: 'itsupport@olivetech.com', // generated ethereal user
+                        pass: '1th3lpd35k' // generated ethereal password
+                    },
+                    tls: {
+                        rejectUnauthorized: false
+                    }
+                });
+
+                // setup email data with unicode symbols
+                let mailOptions = {
+                    from: '"madhavi pathapati ðŸ‘»" <itsupport@olivetech.com>', // sender address
+                    to: 'madhupathapati406@gmail.com', // list of receivers
+                    subject: 'Leave', // Subject line
+                    text: 'Leave Request', // plain text body
+                    html: output // html body
+                };
+
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: %s', info.messageId);
+                    // Preview only available when sending through an Ethereal account
+                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                    // res.render('madhavi',{msg:"Email has been sent"});
+                });
+            } else {
+                // authentication failed
+                res.status(400).send('Username or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function register(req, res) {
+    userService.create(req.body)
+        .then(function () {
+            res.json('success');
+            console.log('new user madhaavi reg');
+            const output = `<p>You have Employees request</p>
+            <h3>Employees Deatils</h3>
+            <ul>
+            <li>Madhavi Employee Details</li>
+            </ul>
+            <h3>Meaasge</h3>
+           
+            `;
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 25,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: 'itsupport@olivetech.com', // generated ethereal user
+                    pass: '1th3lpd35k' // generated ethereal password
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
+
+            // setup email data with unicode symbols
+            let mailOptions = {
+                from: '"madhavi pathapati ðŸ‘»" <itsupport@olivetech.com>', // sender address
+                to: 'madhupathapati406@gmail.com', // list of receivers
+                subject: 'Leave', // Subject line
+                text: 'Leave Request', // plain text body
+                html: output // html body
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                // res.render('madhavi',{msg:"Email has been sent"});
+            });
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getAll(req, res) {
+    userService.getAll()
+        .then(function (users) {
+            res.send(users);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrent(req, res) {
+    userService.getById(req.user.sub)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function update(req, res) {
+    userService.update(req.params._id, req.body)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function _delete(req, res) {
+    userService.delete(req.params._id)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
---
+++ server/services/user.service.js
@@ -0,0 +1,228 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('madhavi');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getAll = getAll;
+service.getById = getById;
+service.create = create;
+service.update = update;
+service.delete = _delete;
+
+module.exports = service;
+
+function authenticate(username, password) {
+    var deferred = Q.defer();
+
+    db.users.findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            // authentication successful
+            deferred.resolve({
+                _id: user._id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                token: jwt.sign({ sub: user._id }, config.secret)
+            });
+        } else {
+            // authentication failed
+            deferred.resolve();
+            app.use('http://localhost:3500/');
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getAll() {
+    var deferred = Q.defer();
+
+    db.users.find().toArray(function (err, users) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        // return users (without hashed passwords)
+        users = _.map(users, function (user) {
+            return _.omit(user, 'hash');
+        });
+
+        deferred.resolve(users);
+    });
+
+    return deferred.promise;
+}
+
+function getById(_id) {
+    var deferred = Q.defer();
+
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user) {
+            // return user (without hashed password)
+            deferred.resolve(_.omit(user, 'hash'));
+        } else {
+            // user not found
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function create(userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.users.findOne(
+        { username: userParam.username },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (user) {
+                // username already exists
+                deferred.reject('Username "' + userParam.username + '" is already taken');
+            } else {
+                createUser();
+            }
+        });
+
+    function createUser() {
+        // set user object to userParam without the cleartext password
+        var user = _.omit(userParam, 'password');
+
+        // add hashed password to user object
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+        const output=`<p>You have Employees request</p>
+        <h3>Employees Deatils</h3>
+        <ul>
+        <li>name:${req.body.name}</li>
+        <li>name:${req.body.comapny}</li>
+        <li>name:${req.body.email}</li>
+        <li>name:${req.body.subject}</li>
+        <li>name:${req.body.body}</li>
+        <li>name:${req.body.leave}</li>
+        </ul>
+        <h3>Meaasge</h3>
+        ${req.body.body}
+        `;
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port:  25,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: 'itsupport@olivetech.com', // generated ethereal user
+                pass: '1th3lpd35k' // generated ethereal password
+            },
+            tls:{
+                rejectUnauthorized:false
+            }
+        });
+        
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"madhavi pathapati ðŸ‘»" <itsupport@olivetech.com>', // sender address
+            to: 'madhupathapati406@gmail.com', // list of receivers
+            subject: 'req.body.subject', // Subject line
+            text: 'Leave Request', // plain text body
+            html: output // html body
+        };
+        
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+           // res.render('employee',{msg:"Email has been sent"});
+        });
+        db.users.insert(
+            user,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user.username !== userParam.username) {
+            // username has changed so check if the new username is already taken
+            db.users.findOne(
+                { username: userParam.username },
+                function (err, user) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (user) {
+                        // username already exists
+                        deferred.reject('Username "' + req.body.username + '" is already taken')
+                    } else {
+                        updateUser();
+                    }
+                });
+        } else {
+            updateUser();
+        }
+    });
+
+    function updateUser() {
+        // fields to update
+        var set = {
+            firstName: userParam.firstName,
+            lastName: userParam.lastName,
+            username: userParam.username,
+        };
+
+        // update password if it was entered
+        if (userParam.password) {
+            set.hash = bcrypt.hashSync(userParam.password, 10);
+        }
+
+        db.users.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.users.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
